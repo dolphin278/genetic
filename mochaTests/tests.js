f2c10bc7760a8406c8428de9d89186efe778a379
DarkRider <mormehtar@gmail.com>
2014-09-07 02:16:37 +0400

    test init reorganization

--- mochaTests/tests.js
+++ mochaTests/tests.js
@@ -7,27 +7,29 @@ var assert = require("assert");
 var Task = require("../lib").Task;
 
 describe("test init", function () {
-    var testData = "Test data";
-    var opts = {
-        popSize: 10,
-        getRandomSolution: sinon.stub()
-    };
-    var task;
+    describe("Main scenario", function () {
+        var testData = "Test data";
+        var opts = {
+            popSize: 10,
+            getRandomSolution: sinon.stub()
+        };
+        var task;
 
-    before(function(done){
-        task = new Task(opts);
-        task.population = [];
-        opts.getRandomSolution.yields(testData);
-        task.init(done);
-    });
+        before(function(done){
+            task = new Task(opts);
+            task.population = [];
+            opts.getRandomSolution.yields(testData);
+            task.init(done);
+        });
 
-    it("Should generate popSize solutions", function(){
-        assert.equal(task.population.length, opts.popSize);
-    });
+        it("Should generate popSize solutions", function(){
+            assert.equal(task.population.length, opts.popSize);
+        });
 
-    it("Should generate solutions with test data", function(){
-        task.population.map(function(solution) {
-            assert.equal(solution, testData);
+        it("Should generate solutions with test data", function(){
+            task.population.map(function(solution) {
+                assert.equal(solution, testData);
+            });
         });
     });
 });
